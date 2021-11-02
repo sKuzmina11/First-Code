0f5c57544920f9bb5b48b372edbf73565a7afbeb
sKuzmina11 <93575810+sKuzmina11@users.noreply.github.com>
2021-11-02 19:17:01 +1100

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,22 @@
+var box
+
+
+function setup() {
+  createCanvas(400,400);
+  box = createSprite(20,20,15,15)
+}
+
+
+function draw() 
+{
+  background(30);
+
+
+  //rect(15,15,10,10) 
+drawSprites()
+
+}
+
+
+
+
